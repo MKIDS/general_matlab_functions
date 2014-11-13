3aa0e5ae42aaad95430ea2de5ca6b025bbc07783
Ross Williamson <rwilliamson@astro.caltech.edu>
2014-11-13 14:33:52 -0800

    Added v2 of files

--- leadingZeros.m
+++ leadingZeros.m
@@ -1,17 +1,16 @@
-function out = leadingZeros(in)
+function out = leadingZeros_v2(in)
 
 in_v = in(:);
 
+v_nz = in_v.bin=='1';
+v_nz = [v_nz ones(length(in)*2,1)];
+
 for i=1:length(in_v)
-    v=in_v(i);
-    v_bin = v.bin;
-    v_nz = v_bin=='1';
-%     v_cs = cumsum(v_nz);
-    loc = find(v_nz, 1, 'first');
-    if(isempty(loc))
-        loc=length(v_bin);
-    end
-    out_v(i) = loc-1;
+    loc(i) = find(v_nz(i,:), 1, 'first');
+%     if(isempty(loc))
+%         loc=length(v_bin);
+%     end
 end
+    out_v = loc-1;
 
 out = reshape(out_v, size(in));
--- socketFillArray.m
+++ socketFillArray.m
@@ -1,10 +1,14 @@
-function out = socketFillArray(fd,command, data)
+function out = socketFillArray(fd,command, data,safe)
+if(~exist('safe','var'))
+    safe=1;
+end
 
 command_header = htonAll(uint32([command, 0, length(data),0]));
 data_conv = htonAll(data);
 out = [command_header; data_conv];
-
-if(fd.BytesToOutput+length(out) + 10000 > fd.OutputBufferSize)
-    error('buffer about to overflow!');
+if(safe)
+    if(fd.BytesToOutput+length(out) + 10000 > fd.OutputBufferSize)
+        error('buffer about to overflow!');
+    end
 end
 fwrite(fd,out);
